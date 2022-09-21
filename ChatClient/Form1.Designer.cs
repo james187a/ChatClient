79fd453b105d2d13e16039aa6385e1b6cf93bea1
James <james187a@gmail.com>
2022-09-21 20:10:34 +0200

    Initial Commit

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(76, 5);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(175, 20);
+            this.txtIP.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(76, 31);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(175, 20);
+            this.txtUser.TabIndex = 1;
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(10, 9);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(51, 13);
+            this.lblIP.TabIndex = 2;
+            this.lblIP.Text = "Server IP";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(10, 34);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(60, 13);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "User Name";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 57);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(319, 342);
+            this.txtLog.TabIndex = 4;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(13, 407);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(238, 20);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(257, 28);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(257, 405);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 439);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Chat Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        private string UserName = "Unknown";
+        private StreamWriter swSender;
+        private StreamReader srReceiver;
+        private TcpClient tcpServer;
+
+        private delegate void UpdateLogCallback(string strMessage);
+        private delegate void CloseConnectionCallback(string strReason);
+
+        private Thread thrMessaging;
+        private IPAddress ipAddr;
+        private bool Connected;
+
+        public Form1()
+        {
+            // On application exit, don't forget to disconnect first
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (Connected == false)
+            {
+                InitializeConnection();
+            }
+            else
+            {
+                CloseConnection("Disconnected at user's request.");
+            }
+        }
+
+        private void InitializeConnection()
+        {
+            ipAddr = IPAddress.Parse(txtIP.Text);
+            tcpServer = new TcpClient();
+            tcpServer.Connect(ipAddr, 1986);
+
+            Connected = true;
+
+            UserName = txtUser.Text;
+
+            txtIP.Enabled = false;
+            txtUser.Enabled = false;
+            txtMessage.Enabled = true;
+            btnSend.Enabled = true;
+            btnConnect.Text = "Disconnect";
+
+            swSender = new StreamWriter(tcpServer.GetStream());
+            swSender.WriteLine(txtUser.Text);
+            swSender.Flush();
+
+            thrMessaging = new Thread(new ThreadStart(ReceiveMessages));
+            thrMessaging.Start();
+        }
+
+        private void ReceiveMessages()
+        {
+            // Receive the response from the server
+            srReceiver = new StreamReader(tcpServer.GetStream());
+            // If the first character of the response is 1, connection was successful
+            string ConResponse = srReceiver.ReadLine();
+            // If the first character is a 1, connection was successful
+            if(ConResponse[0] == '1')
+            {
+                // Update the form to tell it we are now connected
+                this.Invoke(new UpdateLogCallback(this.UpdateLog), new object[] { "Connected Successfully!" });
+            }
+            else // If the first character is not a 1 (probably a 0), the connection was unsuccessful
+            {
+                string Reason = "Not Connected: ";
+                // Extract the reason out of the response message. The reason starts at the 3rd character
+                Reason += ConResponse.Substring(2, ConResponse.Length - 2);
+                // Update the form with the reason why we couldn't connect
+                this.Invoke(new CloseConnectionCallback(this.CloseConnection), new object[] { Reason });
+                // Exit the method
+                return;
+            }
+
+            // While we are successfully connected, read incoming lines from the erver
+            while (Connected)
+            {
+                // Show the messages in the log TextBox
+                Invoke(new UpdateLogCallback(UpdateLog), new object[] { srReceiver.ReadLine() });
+            }
+        }
+
+        // This method is called from a different thread in order to update the log TextBox
+        private void UpdateLog(string strMessage)
+        {
+            // Append text also scrolls the TextBox to the bottom each time
+            txtLog.AppendText(strMessage + "\r\n");
+        }
+
+        // We want to send the message when the Send button is clicked
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        // But we also want to send the message once Enter is pressed
+        private void txtMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // If the key is Enter
+            if(e.KeyChar == (char)13)
+            {
+                SendMessage();
+            }
+        }
+
+        private void SendMessage()
+        {
+            if(txtMessage.Lines.Length >= 1)
+            {
+                swSender.WriteLine(txtMessage.Text);
+                swSender.Flush();
+                txtMessage.Lines = null;
+            }
+
+            txtMessage.Text = "";
+        }
+
+        // Closes a current connection
+        private void CloseConnection(string Reason)
+        {
+            // Show the reason why the connection is ending
+            txtLog.AppendText(Reason + "\r\n");
+            // Enable and disable the appropriate controls on the form
+            txtIP.Enabled = true;
+            txtUser.Enabled = true;
+            txtMessage.Enabled = false;
+            btnSend.Enabled = false;
+            btnConnect.Text = "Connect";
+
+            // Close the objects
+            Connected = false;
+            swSender.Close();
+            srReceiver.Close();
+            tcpServer.Close();
+        }
+
+        // The event handler for application exit
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            if(Connected == true)
+            {
+                // Closes the connetions, streams, etc.
+                Connected = false;
+                swSender.Close();
+                srReceiver.Close();
+                tcpServer.Close();
+            }
+        }
+    }
+}
